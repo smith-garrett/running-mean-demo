4e20ebf913b46160c7ad8dd007c436f567de5383
Garrett Smith <garrettsmith78@gmail.com>
2025-10-04 18:20:26 +0200

    Add dataclass and function for calculating the cumulative mean

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+import dataclasses
+
+
+@dataclasses.dataclass
+class CurrentState:
+    running_mean: float
+    count: int
+
+
+def calculate_new_state(current_state: CurrentState, new_value: float) -> CurrentState:
+    new_count = current_state.count + 1
+    new_mean = (
+        new_value + current_state.count * current_state.running_mean
+    ) / new_count
+    return CurrentState(new_mean, new_count)
+
+
 def main():
     print("Hello from running-mean-demo!")
 
